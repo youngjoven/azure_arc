ad4127cb7cc7d0e440ab604bb906176a2351e28b
likamrat <likamrat@microsoft.com>
2022-01-15 22:30:33 -0800

    testing config permissions

--- azure_jumpstart_arcbox/artifacts/installCAPI.sh
+++ azure_jumpstart_arcbox/artifacts/installCAPI.sh
@@ -204,7 +204,10 @@ sudo -u $adminUsername rm /home/${adminUsername}/.kube/config
 # # cp /var/lib/waagent/custom-script/download/0/$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config.workload
 # cp /var/lib/waagent/custom-script/download/0/$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config.$CLUSTER_NAME
 
-sudo -u $adminUsername cp $CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config
+sudo -u $adminUsername cp /var/lib/waagent/custom-script/download/0/$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config
+sudo -s $adminUsername cp /var/lib/waagent/custom-script/download/0/$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config2
+sudo -u $adminUsername cp ./$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config3
+sudo -s $adminUsername cp ./$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config4
 # cp /var/lib/waagent/custom-script/download/0/$CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config.$CLUSTER_NAME
 # export KUBECONFIG=/var/lib/waagent/custom-script/download/0/$CLUSTER_NAME.kubeconfig
 
