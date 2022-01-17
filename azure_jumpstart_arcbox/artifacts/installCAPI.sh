524b57cc51b86084266a88c54de2e72c394fe109
likamrat <likamrat@microsoft.com>
2022-01-17 07:58:33 -0800

    testing secret

--- azure_jumpstart_arcbox/artifacts/installCAPI.sh
+++ azure_jumpstart_arcbox/artifacts/installCAPI.sh
@@ -164,7 +164,7 @@ metadata:
   name: audit
 type: Opaque
 data:
-  audit-policy.yaml: $(cat "audit-policy.yaml" | base64 -w0)
+  password: $(cat "audit-policy.yaml" | base64 -w0)
   username: $(echo -n "jumpstart" | base64 -w0)
 EOF
 
