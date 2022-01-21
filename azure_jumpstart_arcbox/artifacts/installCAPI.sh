b9e9bc7bf8392eae2004b864a87a27a1a3c0c335
likamrat <likamrat@microsoft.com>
2022-01-21 08:35:30 -0800

    testing az login

--- azure_jumpstart_arcbox/artifacts/installCAPI.sh
+++ azure_jumpstart_arcbox/artifacts/installCAPI.sh
@@ -296,7 +296,7 @@ az login --service-principal --username $SPN_CLIENT_ID --password $SPN_CLIENT_SE
 # Onboarding the cluster to Azure Arc
 echo ""
 workspaceResourceId=$(sudo -u $adminUsername az resource show --resource-group $AZURE_RESOURCE_GROUP --name $logAnalyticsWorkspace --resource-type "Microsoft.OperationalInsights/workspaces" --query id -o tsv)
-sudo -u $adminUsername az connectedk8s connect --name $capiArcDataClusterName --resource-group $AZURE_RESOURCE_GROUP --location $location --tags 'Project=jumpstart_arcbox' --kube-config $CLUSTER_NAME.kubeconfig
+sudo -u $adminUsername az connectedk8s connect --name $capiArcDataClusterName --resource-group $AZURE_RESOURCE_GROUP --location $location --tags 'Project=jumpstart_arcbox' --kube-config $CLUSTER_NAME.kubeconfig --kube-context "$CLUSTER_NAME-admin@$CLUSTER_NAME"
 # Enabling Container Insights and Microsoft Defender for Containers cluster extensions
 echo ""
 sudo -u $adminUsername az k8s-extension create -n "azure-defender" --cluster-name $capiArcDataClusterName --resource-group $AZURE_RESOURCE_GROUP --cluster-type connectedClusters --extension-type Microsoft.AzureDefender.Kubernetes --configuration-settings logAnalyticsWorkspaceResourceID=$workspaceResourceId
